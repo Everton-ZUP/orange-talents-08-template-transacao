fa92fa024b6aef1322ba0c057c3f134a244fcf6a
Everton-ZUP <everton.fabris@zup.com.br>
2021-09-28 13:58:08 -0300

    Spring Initializr

---
+++ src/main/java/br/com/zupedu/trasacao/TrasacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.zupedu.trasacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrasacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrasacaoApplication.class, args);
+	}
+
+}
